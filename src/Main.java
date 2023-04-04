ebbdcd98700d901638184dcc2842fc62642f8df4
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2023-04-04 22:59:20 +0300

    new 2

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,10 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        FamilyTree tree = new FamilyTree();
-        Human Human1 = new Human("man", "Oleg");
-        Human Human2 = new Human("woman", "Alla");
-        Human Human3 = new Human("woman", "Ylya", "Oleg");
-        Human Human4 = new Human("man", "Saha", "Oleg");
-        Human Human5 = new Human("man", "Misha", "Dima");
-        Human Human6 = new Human("woman", "Dasha", "Dima");
-        ArrayList<Human> treeHuman = new ArrayList<>();
-        ArrayList<Object> listParant= new ArrayList<>();
-        treeHuman.add(Human1);
-        treeHuman.add(Human2);
-        treeHuman.add(Human3);
-        treeHuman.add(Human4);
-        treeHuman.add(Human5);
-        treeHuman.add(Human6);
-        System.out.println();
-        for (Human el : treeHuman) {
-            String name_parant = "" ;
-            String name_child = el.getChild();
-            if (!(listParant.contains(name_child))){
-                System.out.println("Ребенок " + name_child);
-                for (Human ec : treeHuman) {
-                     if (ec.getChild().equals(name_child)) {
-                         name_parant += ec.getName_p() + " ";
-                     }
-                }
-
-                System.out.println("Родители " + name_parant);
-                System.out.println();
-                listParant.add(name_child);
-            }
-        }
+        Tree_human tree = new Tree_human();
+        tree.addHuman(new Human("man", "Олег"));
+        tree.addHuman(new Human("woman", "Алла"));
+        tree.addHuman(new Human("man", "Дима",tree.getByName("Олег"), tree.getByName("Алла")));
+        System.out.println(tree.getInfo());
     }
 }
