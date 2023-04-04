b9ab88ad9d4fe3a31c6407f38f9659c9487496d9
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2023-04-04 21:59:01 +0300

    new vers 2

--- src/Tree_human.java
+++ src/Tree_human.java
@@ -4,18 +4,50 @@ import java.util.List;
 public class Tree_human {
     private List<Human> humanList;
 
-    public Tree_human(List<Human> humanList){
+    public Tree_human(List<Human> humanList) {
         this.humanList = humanList;
     }
+
     public Tree_human() {
         this(new ArrayList<>());
     }
-    public void addHuman(Human human){
-        humanList.add(human);
-    }
-    public List<Human> getHumanList() {
-        return humanList;
-    }
 
-
-}
+    public boolean addHuman(Human human) {
+        if (human == null) {
+            return false;
+        }
+        if (humanList.contains(human)) {
+            humanList.add(human);
+            if (human.getFather() != null) {
+                human.getFather().addChild(human);
+            }
+            if (human.getMather() != null) {
+                human.getMather().addChild(human);
+            }
+            return true;
+        }
+        return false;
+    }
+        //public List<Human> getHumanList () {
+         //   return humanList;
+        //}
+    public Human getByName(String name){
+        for (Human human: humanList){
+            if (human.getName_p().equals(name)){
+                return human;
+            }
+        }
+        return null;
+    }
+    public String getInfo(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("дерево ");
+        sb.append(humanList.size());
+        sb.append(" обьектов \n");
+        for (Human human: humanList){
+            sb.append(human.getInfo());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+    }
